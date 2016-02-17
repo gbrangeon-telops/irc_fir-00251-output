ec7c4d0b0aa25a4847f6b39b133b2afd3cc73f71
odionne-telpos <olivier.dionne@telops.com>
2016-02-17 21:39:10 +0000

    ajout des reports dans le script de build
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@18042 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_output_project.tcl
+++ scripts/Build_output_project.tcl
@@ -1,7 +1,7 @@
 set base_dir "d:/Telops/FIR-00251-Output"
 
 
-#Build project PelicanD
+#Build project
 source $base_dir/scripts/gen_project_output.tcl
 #generate bitstream
 launch_runs impl_1 -to_step write_bitstream
@@ -9,9 +9,14 @@ launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 #open implemented design
 open_run impl_1
+#Save Report
+report_timing_summary -file $base_dir/Reports/fir_0251_Output_timing_summary_routed.rpt
+report_clock_utilization -file $base_dir/Reports/fir_0251_Output_clock_utilization_placed.rpt
+report_utilization -file $base_dir/Reports/fir_0251_Output_utilization_placed.rpt
+report_utilization -file $base_dir/Reports/fir_0251_Output_utilization_placed_hier.rpt -hierarchical -hierarchical_depth 5
 #Open Block diagram
-open_bd_design {D:/Telops/FIR-00251-Output/xilinx/fir_0251_Output.srcs/sources_1/bd/core/code.bd}
+open_bd_design $base_dir/xilinx/fir_0251_Output.srcs/sources_1/bd/core/code.bd
 #Export hardware for sdk
-export_hardware [get_files D:/Telops/FIR-00251-Output/xilinx/fir_0251_Output.srcs/sources_1/bd/core/core.bd] [get_runs impl_1] -bitstream -dir D:/Telops/FIR-00251-Output/sdk
+export_hardware [get_files $base_dir/xilinx/fir_0251_Output.srcs/sources_1/bd/core/core.bd] [get_runs impl_1] -bitstream -dir $base_dir/sdk
 # End of this build
 close_project 
