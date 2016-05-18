0514434755cb64b19272958da35186f9b1bce61f
dalain-telpos <denis.alain@telops.com>
2016-05-18 17:49:47 +0000

    Release 1.6.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@18731 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_out_hw_rev = 18549;
-our $rel_out_sw_rev = 18626;
+our $rel_out_sw_rev = 18728;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 18585;
+our $rel_out_common_rev = 18724;
 1;
