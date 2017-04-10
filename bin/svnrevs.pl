61533826463504cbac24eeecf5035e810780cb33
odionne-telpos <olivier.dionne@telops.com>
2017-04-10 17:31:47 +0000

    merge de la branche 2.0.x.x
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@20310 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_out_hw_rev = 18950;
-our $rel_out_sw_rev = 19661;
+our $rel_out_sw_rev = 20252;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 19547;
+our $rel_out_common_rev = 20228;
 1;
