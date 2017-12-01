f8405e4bad5a13761abbcd7654f9d220e5869597
odionne-telpos <olivier.dionne@telops.com>
2017-12-01 18:28:50 +0000

    Release 2.2.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@21215 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs.pl
+++ bin/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_out_hw_rev = 20797;
-our $rel_out_sw_rev = 20799;
+our $rel_out_sw_rev = 21213;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 20796;
+our $rel_out_common_rev = 20950;
 1;
