e42cf70f5fc4b9881338f9fcf531f33b5451cd44
elarouche-telpos <eric.larouche@telops.com>
2019-05-17 13:34:15 +0000

    Release 3.0.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@23564 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_70.pl
+++ bin/svnrevs_70.pl
@@ -1,5 +1,5 @@
-our $rel_out_hw_rev = 22024;
-our $rel_out_sw_rev = 22025;
+our $rel_out_hw_rev = 23514;
+our $rel_out_sw_rev = 23563;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 21997;
+our $rel_out_common_rev = 23551;
 1;
