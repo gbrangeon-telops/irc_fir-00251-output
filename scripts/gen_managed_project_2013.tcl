01a2a60b1fe569c928c95d8ed85a26a8c9c1e69d
elarouche-telpos <eric.larouche@telops.com>
2019-06-11 16:41:01 +0000

    correction du typo dans le script gen_managed_project_2013.tcl
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@23729 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/gen_managed_project_2013.tcl
+++ scripts/gen_managed_project_2013.tcl
@@ -1,8 +1,8 @@
 set proj_name "managed_ip_project"
 set root_dir "D:/Telops/FIR-00251-Output"
 set script_dir $root_dir/scripts
-set proj_dir %root_dir/IP/2013/$FPGA_SIZE/managed_ip_project
-set ip_dir %root_dir/IP/2013/$FPGA_SIZE
+set proj_dir $root_dir/IP/2013/$FPGA_SIZE/managed_ip_project
+set ip_dir $root_dir/IP/2013/$FPGA_SIZE
 # Create project
 create_project $proj_name $proj_dir -ip
 
