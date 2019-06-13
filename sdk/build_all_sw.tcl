349faeec31e1898ec1cc51d70446e6f1c8134ea8
elarouche-telpos <eric.larouche@telops.com>
2019-06-13 12:23:06 +0000

    Correction aux scripts SDK
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@23735 3e98269d-ff51-e449-abca-491a6567d830

---
+++ sdk/build_all_sw.tcl
@@ -0,0 +1,4 @@
+source "D:/Telops/FIR-00251-Output/sdk/sdk_output_cmd.tcl"
+
+create_output_sw
+build_output_sw
--- sdk/sdk_output_cmd.tcl
+++ sdk/sdk_output_cmd.tcl
@@ -1,27 +1,57 @@
+proc create_output_sw {} {
+set current_path [exec pwd]
 
+#Switch directory
+cd "d:/Telops/fir-00251-Output/sdk"
 
-cd "d:/Telops/fir-00251-Output/sdk/"
+#Set workspace
+setws -switch "d:/Telops/fir-00251-Output/sdk"
 
-importprojects D:/Telops/FIR-00251-Output/sdk
+#Create HW projects
 createhw -name hw_platform_70 -hwspec D:/Telops/FIR-00251-Output/sdk/fir_251_output_top_70.hdf
-createbsp -name standalone_bsp_70 -hwproject hw_platform_70 -proc MCU_microblaze_0
 createhw -name hw_platform_160 -hwspec D:/Telops/FIR-00251-Output/sdk/fir_251_output_top_160.hdf
+
+#Create BSP projects
+createbsp -name standalone_bsp_70 -hwproject hw_platform_70 -proc MCU_microblaze_0
 createbsp -name standalone_bsp_160 -hwproject hw_platform_160 -proc MCU_microblaze_0
 
+#Configure BSP projects
 configbsp -bsp standalone_bsp_70 stdin fpga_comm_uart
 configbsp -bsp standalone_bsp_70 stdout fpga_comm_uart
-updatemss -mss standalone_bsp_70/system.mss
 configbsp -bsp standalone_bsp_160 stdin fpga_comm_uart
 configbsp -bsp standalone_bsp_160 stdout fpga_comm_uart
+updatemss -mss standalone_bsp_70/system.mss
 updatemss -mss standalone_bsp_160/system.mss
+
+#Import projects
+importprojects D:/Telops/FIR-00251-Output/sdk
+
+#Configure in release mode
+configapp -app fir_00251_output_70 build-config release
+configapp -app fir_00251_output_160 build-config release
+
+#Return to initial path
+cd $current_path
+}
+
+proc build_output_sw {} {
+set current_path [exec pwd]
+
+#Switch directory
+cd "d:/Telops/fir-00251-Output/sdk"
+
+#Set workspace
+setws -switch "d:/Telops/fir-00251-Output/sdk"
 
 #Clean projects
 projects -clean
 
-configapp -app fir_00251_output_70 build-config release
-configapp -app fir_00251_output_160 build-config release
-
+#Build projects
 projects -build -type bsp -name standalone_bsp_70
-projects -build -type app -name fir_00251_proc_70
+projects -build -type app -name fir_00251_output_70
 projects -build -type bsp -name standalone_bsp_160
-projects -build -type app -name fir_00251_proc_160
+projects -build -type app -name fir_00251_output_160
+
+#Return to initial path
+cd $current_path
+}
