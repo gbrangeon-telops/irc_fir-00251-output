c8fb2d02d2febdc5491f8f007f65b9c54ed8501b
odionne-telpos <olivier.dionne@telops.com>
2019-11-05 19:45:27 +0000

    XML 12.7.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@24420 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Callback.c
+++ src/sw/GC_Callback.c
@@ -36,7 +36,7 @@ extern t_SdiIntf gSdiIntfCtrl;
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam registers callback functions definition.
-// Generated from XML camera definition file version 12.6.255
+// Generated from XML camera definition file version 12.7.0
 // using updateGenICamCallback.m Matlab script.
 
 /**
