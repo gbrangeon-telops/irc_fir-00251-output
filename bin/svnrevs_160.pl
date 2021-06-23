ab576009b9f5796e8c9e0023ea930c95c9cca63b
odionne-telpos <olivier.dionne@telops.com>
2021-06-23 13:50:00 +0000

    Release 5.1.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@26583 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_160.pl
+++ bin/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_out_hw_rev = 26016;
-our $rel_out_sw_rev = 26017;
+our $rel_out_hw_rev = 26536;
+our $rel_out_sw_rev = 26582;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 26004;
+our $rel_out_common_rev = 26551;
 1;
--- src/BuildInfo/BuildInfo.h
+++ src/BuildInfo/BuildInfo.h
@@ -4,7 +4,7 @@
 #ifdef ARCH_FPGA_70
 
 #define SVN_HARDWARE_REV      26536
-#define SVN_SOFTWARE_REV      26375
+#define SVN_SOFTWARE_REV      26582
 #define SVN_BOOTLOADER_REV    0 
 #define SVN_COMMON_REV        26551
 
@@ -23,7 +23,7 @@
 #elif defined(ARCH_FPGA_160)
 
 #define SVN_HARDWARE_REV      26536
-#define SVN_SOFTWARE_REV      26375
+#define SVN_SOFTWARE_REV      26582
 #define SVN_BOOTLOADER_REV    0 
 #define SVN_COMMON_REV        26551
 
