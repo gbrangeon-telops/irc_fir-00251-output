f365268a877a117f704a55f461bd5c0aaa437b1b
elarouche-telpos <eric.larouche@telops.com>
2022-02-18 19:37:31 +0000

    Pre-release 5.1.x.5
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@27188 3e98269d-ff51-e449-abca-491a6567d830

--- IP/70/fwft_afifo_wr36_rd18_d256/fwft_afifo_wr36_rd18_d256_sim_netlist.vhdl
+++ IP/70/fwft_afifo_wr36_rd18_d256/fwft_afifo_wr36_rd18_d256_sim_netlist.vhdl
@@ -1,10 +1,10 @@
 -- Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2016.3 (win64) Build 1682563 Mon Oct 10 19:07:27 MDT 2016
--- Date        : Fri Oct 30 15:28:45 2020
--- Host        : TELOPS250 running 64-bit Service Pack 1  (build 7601)
+-- Date        : Fri Feb 18 13:23:27 2022
+-- Host        : TELOPS258 running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode funcsim
---               d:/Telops/FIR-00251-Output/IP/70/fwft_afifo_wr36_rd18_d256/fwft_afifo_wr36_rd18_d256_sim_netlist.vhdl
+--               D:/Telops/FIR-00251-Output/IP/70/fwft_afifo_wr36_rd18_d256/fwft_afifo_wr36_rd18_d256_sim_netlist.vhdl
 -- Design      : fwft_afifo_wr36_rd18_d256
 -- Purpose     : This VHDL netlist is a functional simulation representation of the design and should not be modified or
 --               synthesized. This netlist cannot be used for SDF annotated simulation.
