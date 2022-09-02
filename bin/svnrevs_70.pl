1075d33d7be793cd41c386d002d1a7bbe715e33d
pcouture-telpos <philippe.couture@telops.com>
2022-09-02 17:33:04 +0000

    Release 5.3.x.4
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@27846 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_70.pl
+++ bin/svnrevs_70.pl
@@ -1,5 +1,5 @@
-our $rel_out_hw_rev = 27410;
-our $rel_out_sw_rev = 27411;
+our $rel_out_hw_rev = 27784;
+our $rel_out_sw_rev = 27845;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 27401;
+our $rel_out_common_rev = 27839;
 1;
--- src/BuildInfo/BuildInfo.h
+++ src/BuildInfo/BuildInfo.h
@@ -4,7 +4,7 @@
 #ifdef ARCH_FPGA_70
 
 #define SVN_HARDWARE_REV      27784
-#define SVN_SOFTWARE_REV      27844
+#define SVN_SOFTWARE_REV      27845
 #define SVN_BOOTLOADER_REV    0 
 #define SVN_COMMON_REV        27839
 
@@ -23,7 +23,7 @@
 #elif defined(ARCH_FPGA_160)
 
 #define SVN_HARDWARE_REV      27784
-#define SVN_SOFTWARE_REV      27844
+#define SVN_SOFTWARE_REV      27845
 #define SVN_BOOTLOADER_REV    0 
 #define SVN_COMMON_REV        27839
 
