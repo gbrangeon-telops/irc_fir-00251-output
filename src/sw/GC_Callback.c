024b22804ea61c2008903d0a8e0aeb140a47abe9
aduchenko-telpos <aduchenko@telops.com>
2022-10-19 20:29:07 +0000

    CLINK DOWNLOAD : Some corrections
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@28003 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Callback.c
+++ src/sw/GC_Callback.c
@@ -217,8 +217,6 @@ void GC_AcquisitionStartCallback(gcCallbackPhase_t phase, gcCallbackAccess_t acc
  */
 void GC_AcquisitionStopCallback(gcCallbackPhase_t phase, gcCallbackAccess_t access)
 {
-   extern t_ClinkConfig gClinkCtrl;
-
    if ((phase == GCCP_AFTER) && (access == GCCA_WRITE))
    {
       if (gcRegsData.AcquisitionStop)
