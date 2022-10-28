cb2e5c659189c9f4be6ebc9eb884f5eb4cc20941
pcouture-telpos <philippe.couture@telops.com>
2022-10-28 16:16:50 +0000

    Release 5.3.x.6
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@28035 3e98269d-ff51-e449-abca-491a6567d830

--- bin/svnrevs_160.pl
+++ bin/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_out_hw_rev = 27784;
-our $rel_out_sw_rev = 27845;
+our $rel_out_hw_rev = 28033;
+our $rel_out_sw_rev = 28034;
 our $rel_out_boot_rev = 0;
-our $rel_out_common_rev = 27839;
+our $rel_out_common_rev = 28031;
 1;
