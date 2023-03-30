70993ccdde11be9e6c570d18d27b319fdf943df1
odionne-telpos <olivier.dionne@telops.com>
2023-03-30 17:19:07 +0000

    XML 13.3.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@28704 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Registers.h
+++ src/sw/GC_Registers.h
@@ -24,10 +24,10 @@ extern uint8_t gAcquisitionStarted;
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 13.2.0
+// Generated from XML camera definition file version 13.3.0
 // using generateGenICamCLib.m Matlab script.
 
-#if ((GC_XMLMAJORVERSION != 13) || (GC_XMLMINORVERSION != 2) || (GC_XMLSUBMINORVERSION != 0))
+#if ((GC_XMLMAJORVERSION != 13) || (GC_XMLMINORVERSION != 3) || (GC_XMLSUBMINORVERSION != 0))
 #error "XML version mismatch."
 #endif
 
