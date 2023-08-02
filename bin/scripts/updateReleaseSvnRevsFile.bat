a51842a6947ac74fe9a0080f23bc1875798af2dc
apullara-telpos <alexis.pullara@telops.com>
2023-08-02 13:54:09 +0000

    oublie maj script updateReleasesvnrev for tcl
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Output/trunk@29051 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/updateReleaseSvnRevsFile.bat
+++ bin/scripts/updateReleaseSvnRevsFile.bat
@@ -1,13 +1,13 @@
 rem Clean up
 del %revFile%
 
-echo our $rel_out_hw_rev = $WCREV$;> %revFile%
+echo set rel_out_hw_rev $WCREV$> %revFile%
 %svn_subwcrev% %hwFile% %revFile% %revFile%
 
-echo our $rel_out_sw_rev = $WCREV$;>> %revFile%
+echo set rel_out_sw_rev $WCREV$>> %revFile%
 %svn_subwcrev% %elfFile% %revFile% %revFile%
 
-echo our $rel_out_boot_rev = 0;>> %revFile%
+echo set rel_out_boot_rev 0 >> %revFile%
 
-echo our $rel_out_common_rev = $WCREV$;>> %revFile%
+echo set rel_out_common_rev $WCREV$>> %revFile%
 %svn_subwcrev% %commonDir% %revFile% %revFile%
